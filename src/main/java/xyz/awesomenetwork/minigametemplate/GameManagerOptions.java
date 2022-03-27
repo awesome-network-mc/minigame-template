4974c50098e5ba5f1348d0af46081ec3f45bfd3c
Dominic Beeson <dominicraebeeson@gmail.com>
2022-03-27 17:20:56 +0100

    better game options builder

--- src/main/java/xyz/awesomenetwork/minigametemplate/GameManagerOptions.java
+++ src/main/java/xyz/awesomenetwork/minigametemplate/GameManagerOptions.java
@@ -1,25 +1,93 @@
 package xyz.awesomenetwork.minigametemplate;
 
 public class GameManagerOptions {
-
     public final boolean joinStartedGame, autoSpectateOnDeath, autoRespawn, displayDeathMessages;
     public final int maxGameTimeSeconds, maxPlayers, minStartPlayers, gameEndTimeSeconds, autoRespawnTimerSeconds, combatTagTimeSeconds, gameStartCountdownSeconds;
 
-    public GameManagerOptions() {
-        this(false, true, 0, 0, 2, 10, false, 5, 5, true, 60);
+    public GameManagerOptions(GameManagerOptionsBuilder options) {
+        this.joinStartedGame = options.joinStartedGame;
+        this.autoSpectateOnDeath = options.autoSpectateOnDeath;
+        this.autoRespawn = options.autoRespawn;
+        this.displayDeathMessages = options.displayDeathMessages;
+        this.maxGameTimeSeconds = options.maxGameTimeSeconds;
+        this.maxPlayers = options.maxPlayers;
+        this.minStartPlayers = options.minStartPlayers;
+        this.gameEndTimeSeconds = options.gameEndTimeSeconds;
+        this.autoRespawnTimerSeconds = options.autoRespawnTimerSeconds;
+        this.combatTagTimeSeconds = options.combatTagTimeSeconds;
+        this.gameStartCountdownSeconds = options.gameStartCountdownSeconds;
     }
 
-    public GameManagerOptions(boolean joinStartedGame, boolean autoSpectateOnDeath, int maxGameTimeSeconds, int maxPlayers, int minStartPlayers, int gameEndTimeSeconds, boolean autoRespawn, int autoRespawnTimerSeconds, int combatTagTimeSeconds, boolean displayDeathMessages, int gameStartCountdownSeconds) {
-        this.joinStartedGame = joinStartedGame;
-        this.autoSpectateOnDeath = autoSpectateOnDeath;
-        this.maxGameTimeSeconds = maxGameTimeSeconds;
-        this.maxPlayers = maxPlayers;
-        this.minStartPlayers = minStartPlayers;
-        this.gameEndTimeSeconds = gameEndTimeSeconds;
-        this.autoRespawn = autoRespawn;
-        this.autoRespawnTimerSeconds = autoRespawnTimerSeconds;
-        this.combatTagTimeSeconds = combatTagTimeSeconds;
-        this.displayDeathMessages = displayDeathMessages;
-        this.gameStartCountdownSeconds = gameStartCountdownSeconds;
+    public static class GameManagerOptionsBuilder {
+        private boolean joinStartedGame = false;
+        private boolean autoSpectateOnDeath = true;
+        private boolean autoRespawn = false;
+        private boolean displayDeathMessages = true;
+        private int maxGameTimeSeconds = 0;
+        private int maxPlayers = 100;
+        private int minStartPlayers = 2;
+        private int gameEndTimeSeconds = 10;
+        private int autoRespawnTimerSeconds = 5;
+        private int combatTagTimeSeconds = 5;
+        private int gameStartCountdownSeconds = 60;
+
+        public GameManagerOptionsBuilder canJoinStartedGame(boolean joinStartedGame) {
+            this.joinStartedGame = joinStartedGame;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder shouldAutoSpectateOnDeath(boolean autoSpectateOnDeath) {
+            this.autoSpectateOnDeath = autoSpectateOnDeath;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder canAutoRespawn(boolean autoRespawn) {
+            this.autoRespawn = autoRespawn;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder displayDeathMessages(boolean displayDeathMessages) {
+            this.displayDeathMessages = displayDeathMessages;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder maxGameTimerSeconds(int maxGameTimeSeconds) {
+            this.maxGameTimeSeconds = maxGameTimeSeconds;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder maxIngamePlayers(int maxPlayers) {
+            this.maxPlayers = maxPlayers;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder playersToStartGameCountdown(int minStartPlayers) {
+            this.minStartPlayers = minStartPlayers;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder gameEndTimerSeconds(int gameEndTimeSeconds) {
+            this.gameEndTimeSeconds = gameEndTimeSeconds;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder autoRespawnTimerSeconds(int autoRespawnTimerSeconds) {
+            this.autoRespawnTimerSeconds = autoRespawnTimerSeconds;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder combatTagTimeSeconds(int combatTagTimeSeconds) {
+            this.combatTagTimeSeconds = combatTagTimeSeconds;
+            return this;
+        }
+
+        public GameManagerOptionsBuilder gameStartCountdownTimeSeconds(int gameStartCountdownSeconds) {
+            this.gameStartCountdownSeconds = gameStartCountdownSeconds;
+            return this;
+        }
+
+        public GameManagerOptions build() {
+            return new GameManagerOptions(this);
+        }
     }
 }
